7e7356f56e9e7eec6da1d8773d225e7e9448b275
Ben Hoover <benhoover34@gmail.com>
2023-10-31 22:00:00 -0400

    Fix zooming out of hero

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -25,7 +25,7 @@
   .hero-image {
     background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
       url("/img/dalle_synapses.jpg");
-    height: 70%;
+    height: 700px;
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
@@ -53,7 +53,7 @@
 
   @media only screen and (max-width: 768px) {
     .hero-image {
-      height: 90%;
+      height: 700px;
     }
 
     .hero-text {
